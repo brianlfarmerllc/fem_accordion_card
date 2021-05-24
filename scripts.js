2279e2971834116174281fc0c7967563ac2d8d55
Brian Farmer <brianlfarmerllc@gmail.com>
2021-05-24 14:28:44 -0500

    add media query for mobil views

--- scripts.js
+++ scripts.js
@@ -7,14 +7,16 @@ const header4 = document.querySelectorAll("h4");
 
 function viewParagraph() {
   // Var
+  const arrowImg = this.closest("div").children[1];
   const pText = this.closest(".text-container").children[1];
   const h4Text = this.closest("div").children[0];
+  console.log(this.className);
 
-  if (this.className != "arrow active") {
+  if (this.className != "dark") {
     arrow.forEach((el) => el.classList.remove("active"));
     paragraph.forEach((el) => el.classList.remove("view"));
     header4.forEach((el) => el.classList.remove("dark"));
-    this.classList.toggle("active");
+    arrowImg.classList.toggle("active");
     h4Text.classList.toggle("dark");
     pText.classList.toggle("view");
   } else {
@@ -27,5 +29,8 @@ function viewParagraph() {
 // Events Handelers
 
 arrow.forEach((el) => {
+  el.addEventListener("click", viewParagraph);
+});
+header4.forEach((el) => {
   el.addEventListener("click", viewParagraph);
 });
